e4664fafdf058891e1a23b7388c655fb672f2b44
Ashutosh Chandra <ashutosh@Ashutoshs-MacBook-Pro.local>
2026-02-26 12:26:59 +0530

    Fix preview img

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import Header from '~/components/common/header.vue';
-import logo from '../assets/img/logo/metaImg.png'
+import logo from '../../public/metaImg.png'
 useMeta({
   title: 'Eco-friendly disposables | Dharitrie ',
   description: "Discover Dharitrie's premium range of eco-friendly dinnerware. Our utensils are 100% biodegradable, toxin-free, and designed to return to the Earth",
-  image: logo
+  image: logo,
+  url: 'https://dharitrie.com'
 
 })
 
