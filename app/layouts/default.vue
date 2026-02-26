dfa026824e733c0fc109c2b185020ac8ee2eed8c
Ashutosh Chandra <ashutosh@Ashutoshs-MacBook-Pro.local>
2026-02-26 12:18:32 +0530

    Fix meta link preview

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import Header from '~/components/common/header.vue';
-import logo from '../assets/img/logo/logo.png'
+import logo from '../assets/img/logo/metaImg.png'
 useMeta({
   title: 'Eco-friendly disposables | Dharitrie ',
-  description: "Discover Dharitrie's premium range of eco-friendly dinnerware. Crafted from solid agricultural residue, our utensils are 100% biodegradable, toxin-free, and designed to return to the Earth",
+  description: "Discover Dharitrie's premium range of eco-friendly dinnerware. Our utensils are 100% biodegradable, toxin-free, and designed to return to the Earth",
   image: logo
 
 })
